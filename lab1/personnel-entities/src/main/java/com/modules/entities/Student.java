9ef63d4e08e87fe6109f1a428ece2eeb97dd4129
Fadel Akram <af8356207@gmail.com>
2025-10-30 18:22:04 +0100

    fixed Serializability Error for Student Class by impplementing Serializable interface

--- lab1/personnel-entities/src/main/java/com/modules/entities/Student.java
+++ lab1/personnel-entities/src/main/java/com/modules/entities/Student.java
@@ -1,7 +1,9 @@
 package com.modules.entities;
 
 
-public class Student {
+import java.io.Serializable;
+
+public class Student implements Serializable  {
     private static final long serialVersionUID = 1L;
 
     
